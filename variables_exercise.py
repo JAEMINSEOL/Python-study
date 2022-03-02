8bd1415b9c9a0edb90a5a297c54e9540b309ecb5
JAEMINSEOL <43691888+JAEMINSEOL@users.noreply.github.com>
2022-03-02 17:47:22 +0900

    Create variables_exercise.py

---
+++ variables_exercise.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 11:35:16 2022
+
+@author:Jaemin
+https://www.youtube.com/watch?v=8_VWnRvbGPs
+"""
+c=1+2j
+c.real()
+c.imag()
+
+name='jaemin'
+name.capitalize()
+name.split('e')
+
+s = 'abc\def'
+s
+print(s)
+s='abc\\def'
+text = 'I\'m Jaemin'
+text
+print(text)
+dir_path = 'C:\\Python\\myproject'
+print(dir_path)
+
+s1='python'
+s1 = s1+ ' project'
+s1=s1*3
+
+a=int(input('Enter value: '))
+
+dir()
+globals()
+locals()
+del a
+a=3
+b=3
+a.__eq__(b)
+a,b,c = 5,10,20
+a,b,c=b,a,c # 주석 달기 연습
