e77a0847466a6d3f760bd99c3f5cea5c7f0031ac
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2024-05-10 17:24:47 +0900

    Add files via upload

---
+++ 20240503.py
@@ -0,0 +1,38 @@
+print(5%3)
+print(1!=3)
+print(2+3*4)
+print((2+3)*4)
+number = 2+3*4
+print(number)
+number = number+2
+print(number)
+number+=2
+print(number)
+number*=2
+print(number)
+number/=2
+print(number)
+number-=2
+print(number)
+number%=3
+print(number)
+print(abs(-5))
+print(pow(4,2))
+print(max(5,12,34))
+print(min(5,13,-3,4.5))
+print(round(3.1423321))
+print(round(-4.33342))
+
+from math import *
+print(floor(4.99))
+print(floor(-4.556))
+print(ceil(3.113))
+print(sqrt(16))
+print(sqrt(-44))
+
+from random import *
+print(random())
+print(randint(2,30)) # 2~30 이하의 임의의 정수 생성
+print(int(random()*10))
+print(int(random()*45)+1)
+print(randrange(1,46)) # 1~46 미만의 임의의 값 생성
