74a76d492dd843b0cf1f5531eb01ef25faa85d53
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2024-05-28 16:07:48 +0900

    Update 20240527.py

--- 20240527.py
+++ 20240527.py
@@ -21,3 +21,69 @@ elif 0<=temp < 10:
 else:
     print('too cold')
 # %%
+for i in (range(4)):
+    print(f'num : {i}')
+# %%
+starbucks = ['Ironman','Thor','Groot']
+for customer in starbucks:
+    print(f'{customer}, your coffee is ready ')
+# %%
+# while
+customer = 'Thor'
+index=5
+while index>=1:
+    print(f'{customer}, your coffee is ready. {index} left')
+    index -= 1
+    if index==0:
+        print('trash coffee')
+
+# %%
+customer = 'Ironman'
+person='none'
+index=1
+while True:
+    print(f'{customer}, your coffee is ready. {index} times calling')
+    index +=1
+    person = input("Customer? :")
+    if person == customer:
+        break
+
+#%%
+absent=  [2,5]
+no_book = [7]
+for student in range(1,11):
+    if student in absent:
+        print(f'{student} is missing')
+    elif student in no_book:
+        print(f'class is over. {student}, come to the principal\'s office')
+        break
+# %%
+students=[1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print(students)
+
+#%%
+students = ['Iron man','Thor','Groot']
+# students = [len(i) for i in students]
+print(students)
+
+students = [i.upper() for i in students]
+print(students)
+
+
+#%%
+# quiz
+from random import *
+
+num=0
+for i in range(1,51):
+    time = randrange(5,51)
+    if 5<=time<=15:
+        allowed='O'; num+=1
+    else:
+        allowed=' '
+    print(f'[{allowed}] {i} 번째 손님 (소요시간 : {time}분)')
+print(f'총 탑승 승객 : {num}분')
+
+# %%
